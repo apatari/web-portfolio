1d64a9df3777e15fcaf80796a482dff67f0e37f0
Andy Patari <andy.patari@gmail.com>
2024-01-29 19:51:46 -0500

    update bio

--- src/components/About.js
+++ src/components/About.js
@@ -24,13 +24,14 @@ function About() {
                         <h3 className="text-secondary m-1" >About Me</h3>
                         <div className="fs-5 text-light m-2"  >
 
-                            <p> An inquisitive and determined problem solver, I was fortunate enough to spend 11 
-                                years working as a high school 
+                            <p> I'm a software engineer with full-stack web development experience.  I especially enjoy 
+                                building RESTful APIs and robust backends.  
+                                In my previous career I spent 11 years working as a high school 
                             science teacher at a wonderful public school.  I believe that the times of transition 
-                            in my life are especially dense with memory and learning. Going to graduate school, being 
-                            a first-year teacher, becoming a father - all of these made me a better, more resilient 
-                            person.  This spirit of growth has brought me to my studies at the Flatiron School to 
-                            prepare for a new career in software engineering.  I am excited to continue developing a
+                            in my life are especially dense with memory and learning. Serving in the Peace Corps, going to graduate school, 
+                            being a first-year teacher, becoming a father - all of these made me a better, more resilient 
+                            person.  This spirit of growth has brought me to my studies at the Flatiron School where I 
+                            studied software engineering.  I am excited to continue developing a
                             set of skills that allow me to create authentic, technically complex work.</p>
                         </div>
                     </Row>
