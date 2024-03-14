1af837dce7159aff377825d29c552ea30d62b82d
Andy Patari <andy.patari@gmail.com>
2024-03-14 13:50:22 -0400

    add 3rd project card

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -15,8 +15,8 @@ function Projects() {
                 <h3 className="ps-5" >Projects</h3>
                 <p className="fs-3 ps-5 " >Click on any project image to see a video demo</p>
             </Row>
-            <Row className="mb-3 text-dark" data-bs-theme='dark' >
-            <Card style={{width: '28rem'}} className="m-3 p-2 rounded" >
+            <Row className="mb-3 text-dark p-2" data-bs-theme='dark' >
+            <Card style={{width: '28rem'}} className="m-2  p-2 rounded" >
                 <a href="https://www.youtube.com/watch?v=xzJ9E-R6PK4&t=96s" target="blank" > <Card.Img variant="top" src={NNSSnew} /></a>
                 <Card.Body>
                     <Card.Title>Nordic Nexus</Card.Title>
@@ -29,7 +29,7 @@ function Projects() {
                 </Card.Body>
             </Card>
 
-            <Card style={{width: '28rem'}} className="m-3 p-2 rounded" >
+            <Card style={{width: '28rem'}} className="m-2 p-2 rounded" >
                 <a href="https://www.youtube.com/watch?v=RyR3Dh2zjNc" target="blank" > <Card.Img variant="top" src={Art} /></a>
                 <Card.Body>
                     <Card.Title>Art Showcase</Card.Title>
@@ -38,6 +38,19 @@ function Projects() {
                         A website to show off the weavings that my mom has for sale. Features include file uploads, form-generated emails, and image storage. 
                     </Card.Text>
                     <a href="https://github.com/apatari/art-store" className="fs-3" target="blank" > GitHub </a>
+                    
+                </Card.Body>
+            </Card>
+
+            <Card style={{width: '28rem'}} className="m-2 p-2 rounded" >
+                <a href="https://www.youtube.com/watch?v=aTaVnL6IR4M" target="blank" > <Card.Img variant="top" src={SRSSnew} /></a>
+                <Card.Body>
+                    <Card.Title>Squawk Rack</Card.Title>
+                  
+                    <Card.Text className="fs-4" >
+                        Create, share, review, and comment on resistance training workout plans. 
+                    </Card.Text>
+                    <a href="https://github.com/apatari/squawk-rack" className="fs-3" target="blank" > GitHub </a>
                     
                 </Card.Body>
             </Card>
